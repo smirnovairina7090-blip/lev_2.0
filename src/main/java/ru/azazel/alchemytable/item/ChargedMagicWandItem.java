881fcdd350d4f2632564e6175b50b804ec8e2824
smirnovairina7090-blip <smirnova.irina7090@gmail.com>
2026-09-07 11:19:23 +0300

    Make charged wand shoot light projectiles

--- src/main/java/ru/azazel/alchemytable/item/ChargedMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/ChargedMagicWandItem.java
@@ -1,7 +1,12 @@
 package ru.azazel.alchemytable.item;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import ru.azazel.alchemytable.entity.LightProjectile;
 
 public class ChargedMagicWandItem extends Item {
 
@@ -9,6 +14,23 @@ public class ChargedMagicWandItem extends Item {
         super(properties);
     }
 
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (!level.isClientSide) {
+            LightProjectile projectile = new LightProjectile(level, player);
+            projectile.shootFromRotation(
+                    player,
+                    player.getXRot(),
+                    player.getYRot(),
+                    0.0F,
+                    1.5F,
+                    1.0F
+            );
+            level.addFreshEntity(projectile);
+        }
+
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+    }
 }
-
-
