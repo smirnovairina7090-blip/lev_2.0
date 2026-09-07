27bf63d55e99101c1677b5c012e405923ccc25ff
smirnovairina7090-blip <smirnova.irina7090@gmail.com>
2026-09-07 11:19:31 +0300

    Render light projectile on client

--- src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
+++ src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
@@ -2,10 +2,13 @@ package ru.azazel.alchemytable.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import ru.azazel.alchemytable.block.ModBlocks;
 import ru.azazel.alchemytable.client.screen.AlchemyTableScreen;
+import ru.azazel.alchemytable.entity.ModEntities;
 import ru.azazel.alchemytable.menu.ModMenuTypes;
 
 public class AzazelSAlchemyTableClient implements ClientModInitializer {
@@ -20,6 +23,11 @@ public class AzazelSAlchemyTableClient implements ClientModInitializer {
         MenuScreens.register(
                 ModMenuTypes.ALCHEMY_TABLE_MENU,
                 AlchemyTableScreen::new
+        );
+
+        EntityRendererRegistry.register(
+                ModEntities.LIGHT_PROJECTILE,
+                ThrownItemRenderer::new
         );
     }
 }
