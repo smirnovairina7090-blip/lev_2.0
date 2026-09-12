62d7bba1752f23901a10076e7e7b8b4672b32771
smirnovairina7090-blip <smirnova.irina7090@gmail.com>
2026-09-12 11:16:46 +0300

    Register all charged wand variants

--- src/main/java/ru/azazel/alchemytable/item/ModItems.java
+++ src/main/java/ru/azazel/alchemytable/item/ModItems.java
@@ -21,12 +21,47 @@ public final class ModItems {
             new ChargedMagicWandItem(new Item.Properties().stacksTo(1))
     );
 
+    public static final Item FIRE_MAGIC_WAND = Registry.register(
+            BuiltInRegistries.ITEM,
+            AzazelSAlchemyTable.id("fire_magic_wand"),
+            new Item(new Item.Properties().stacksTo(1))
+    );
+
+    public static final Item WATER_MAGIC_WAND = Registry.register(
+            BuiltInRegistries.ITEM,
+            AzazelSAlchemyTable.id("water_magic_wand"),
+            new Item(new Item.Properties().stacksTo(1))
+    );
+
+    public static final Item WIND_MAGIC_WAND = Registry.register(
+            BuiltInRegistries.ITEM,
+            AzazelSAlchemyTable.id("wind_magic_wand"),
+            new Item(new Item.Properties().stacksTo(1))
+    );
+
+    public static final Item REDSTONE_MAGIC_WAND = Registry.register(
+            BuiltInRegistries.ITEM,
+            AzazelSAlchemyTable.id("redstone_magic_wand"),
+            new Item(new Item.Properties().stacksTo(1))
+    );
+
+    public static final Item GLOWING_MAGIC_WAND = Registry.register(
+            BuiltInRegistries.ITEM,
+            AzazelSAlchemyTable.id("glowing_magic_wand"),
+            new Item(new Item.Properties().stacksTo(1))
+    );
+
     public static void registerModItems() {
         ItemGroupEvents.modifyEntriesEvent(
                 CreativeModeTabs.TOOLS_AND_UTILITIES
         ).register(entries -> {
             entries.accept(MAGIC_WAND);
             entries.accept(LIGHT_MAGIC_WAND);
+            entries.accept(FIRE_MAGIC_WAND);
+            entries.accept(WATER_MAGIC_WAND);
+            entries.accept(WIND_MAGIC_WAND);
+            entries.accept(REDSTONE_MAGIC_WAND);
+            entries.accept(GLOWING_MAGIC_WAND);
         });
     }
 
