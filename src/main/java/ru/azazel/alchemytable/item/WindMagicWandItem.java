d60ea9b1767e082f9972b9c011dac51db0400cd3
smirnovairina7090-blip <smirnova.irina7090@gmail.com>
2026-09-18 11:14:54 +0300

    Add wind magic wand item

---
+++ src/main/java/ru/azazel/alchemytable/item/WindMagicWandItem.java
@@ -0,0 +1,36 @@
+package ru.azazel.alchemytable.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import ru.azazel.alchemytable.entity.WindProjectile;
+
+public class WindMagicWandItem extends Item {
+
+    public WindMagicWandItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (!level.isClientSide) {
+            WindProjectile projectile = new WindProjectile(level, player);
+            projectile.shootFromRotation(
+                    player,
+                    player.getXRot(),
+                    player.getYRot(),
+                    0.0F,
+                    1.5F,
+                    1.0F
+            );
+            level.addFreshEntity(projectile);
+        }
+
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+    }
+}
